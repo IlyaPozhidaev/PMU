968d7f8eabc323b834bad8701044dc478c0755c3
Denis <99001435+sinedfq@users.noreply.github.com>
2024-10-10 17:22:46 +0700

    Add CubeClass and displaying a Cube on the screen

--- ReferenceBook/app/src/main/java/com/example/referencebook/Render.kt
+++ ReferenceBook/app/src/main/java/com/example/referencebook/Render.kt
@@ -9,6 +9,7 @@ import javax.microedition.khronos.opengles.GL10
 
 class Render(context: Context) : GLSurfaceView.Renderer {
     private val texturedSquare: Square = Square(context)
+    private val cube = Cube()
 
     override fun onSurfaceCreated(gl: GL10, config: EGLConfig?) {
         gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
@@ -33,5 +34,12 @@ class Render(context: Context) : GLSurfaceView.Renderer {
         gl.glTranslatef(0.0f, 0.0f, -5.0f)
         gl.glScalef(2.0f, 2.0f, 1.0f)
         texturedSquare.draw(gl)
+
+        gl.glPushMatrix()
+        gl.glTranslatef(0.0f, 0.0f, 1.0f)
+        gl.glScalef(0.2f, 0.2f, 0.2f)
+        gl.glRotatef(30.0f, 1.0f, 0.5f, 0.0f)
+        cube.draw(gl)
+        gl.glPopMatrix()
     }
 }
